795221f192441269e0c14a92bf2805fa071f3a93
Roman Doroshenko <coachandconsult3@gmail.com>
2020-07-27 08:29:43 +0300

    task

--- src/module1/part2/Main4.java
+++ src/module1/part2/Main4.java
@@ -1,14 +1,37 @@
 package module1.part2;
+
+import java.util.Scanner;
+
 //Заданы размеры А, В прямоугольного отверстия и размеры х, у, z кирпича. Определить, пройдет ли кирпич через
 //отверстие.
 public class Main4 {
     public static void main(String[] args) {
-        int foramenA = 5;
-        int foramenB = 15;
+        int foramenA;
+        int foramenB;
 
-        int length = 14;
-        int height = 5;
-        int width = 7;
+        int length = 25;
+        int height = 6;
+        int width = 12;
+        System.out.print(">>");
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.print(">>");
+        }
+        foramenA = scanner.nextInt();
+        System.out.println();
+        System.out.print(">>");
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.println();
+            System.out.print(">>");
+        }
+        foramenB = scanner.nextInt();
+
+        if (foramenA > length && foramenB > width || foramenA > height && foramenB > width ||
+                foramenA > width && foramenB > height) {
+            System.out.println("The brick will go through the hole");
+        } else System.out.println("The brick will not go through the hole");
 
 
     }
