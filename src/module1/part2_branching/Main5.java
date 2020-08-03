f5520e58342ed7e6423c2cb8b00118d687a143ec
Roman Doroshenko <coachandconsult3@gmail.com>
2020-08-03 13:21:42 +0300

    задачи по курсу

---
+++ src/module1/part2_branching/Main5.java
@@ -0,0 +1,31 @@
+package module1.part2_branching;
+
+// вычислить значение функции x digr2 - 3x + 9 при x <= 3;
+// 1/x digr3 + 6 if x > 3;
+// вариант решения с использованием цикла
+public class Main5 {
+
+    public static void main(String[] args) {
+        double x = 3;
+        double y;
+        double z = 4;
+
+        while (x <= 3 && x >= -6) {
+            y = Math.pow(x, 2) - 3 * x + 9;
+            x--;
+            System.out.print(" x = " + x + " " + " y = " + y + "; ");
+
+        }
+
+        System.out.println();
+
+        while (z > 3 && z < 14) {
+            y = 1 / Math.pow(z, 3) + 6;
+            z++;
+            System.out.print(" x = " + z + " " + " y = " + y + ";");
+
+        }
+
+
+    }
+}
