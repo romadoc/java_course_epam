46fb7432c332e69cb4c8b70580f30eadbfc4b8b7
Roman Doroshenko <coachandconsult3@gmail.com>
2020-09-25 20:10:01 +0300

    задачи по курсу

---
+++ src/module3/part1_strings_as_massive/Main5.java
@@ -0,0 +1,57 @@
+package module3.part1_strings_as_massive;
+/*
+Удалить в строке все лишние пробелы, то есть серии подряд идущих пробелов заменить на одиночные пробелы.
+Крайние пробелы в строке удалить.
+ */
+
+public class Main5 {
+    public static void main(String[] args) {
+        String s;
+        s = " string test  space remover  hope i   can do it ";
+        printString("start string", s);
+        solver(s);
+
+
+    }
+
+    private static String trimmer(char[]charsIn) {
+        //String[] arrayStr = s.split(" ");
+        String s1 = "";
+
+        for (int i = 0; i < charsIn.length; i++) {
+            if (!isCharSpace(charsIn[i])) {
+                s1 = s1 + charsIn[i];
+            }
+            /*
+            char[] chars = arrayStr[i].toCharArray();
+            for (int j = 0; j < chars.length; j++) {
+
+            }
+
+             */
+        }
+      return s1;
+    }
+
+    private static boolean isCharSpace(char ch) {
+        boolean isSpace = false;
+        if (ch == ' ') {
+            isSpace = true;
+        }
+        return isSpace;
+    }
+
+    private static void printString(String answer, String stringIn) {
+
+        System.out.println(answer + ": >>" + stringIn);
+    }
+    private static void solver(String s){
+        String[]arrayString = s.split(" ");
+        String string = "";
+
+        for(int i = 0; i < arrayString.length; i++){
+           string = string + " " + trimmer(arrayString[i].toCharArray());
+        }
+        System.out.print(string);
+    }
+}
