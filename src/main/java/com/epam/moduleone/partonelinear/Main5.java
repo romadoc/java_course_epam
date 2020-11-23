5d8a56b41a029e161eb08f89d578f97d6cc60d34
Roman Doroshenko <coachandconsult3@gmail.com>
2020-11-23 12:30:40 +0300

    задачи по курсу 4 модуль

--- src/main/java/com/epam/moduleone/partonelinear/Main5.java
+++ src/main/java/com/epam/moduleone/partonelinear/Main5.java
@@ -1,4 +1,4 @@
-package com.epam.module1.part1_linear;
+package com.epam.moduleone.partonelinear;
 
 import java.util.Scanner;
 
@@ -10,21 +10,18 @@ public class Main5 {
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Type the number of seconds (in natural number form) >>");
         int seconds = scanner.nextInt();
         int hours = seconds / 3600;
         int minutes = (seconds - hours * 3600) / 60;
         int seconds1 = seconds - hours * 3600 - minutes * 60;
         StringBuilder outputDates = new StringBuilder();
         outputDates.append(hours).append("ч ").append(minutes).append("мин ").append(seconds1).append("с.");
+
         if (hours < 10) {
             outputDates.insert(0, "0");
         }
-        if (minutes < 10) {
-            outputDates.insert(4, "0");
-        }
-        if (seconds1 < 10) {
-            outputDates.insert(10, "0");
-        }
+
         System.out.println(outputDates);
 
     }
