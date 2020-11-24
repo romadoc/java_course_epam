65789b9ce3afb9f6b97f6f20718dea4187269092
Roman Doroshenko <coachandconsult3@gmail.com>
2020-11-24 15:22:42 +0300

    the first module - linear fixed

---
+++ src/main/java/com/epam/moduleone/parttwobranching/Main1.java
@@ -0,0 +1,39 @@
+package com.epam.moduleone.parttwobranching;
+
+import java.util.Scanner;
+
+public class Main1 {
+    //  Даны два угла треугольника (в градусах). Определить, существует ли такой треугольник, и если да, то будет ли
+    //  он прямоугольным.
+
+    public static void main(String[] args) {
+        float angleX;
+        float angleY;
+        angleX = angleEnter("enter the first angle (in degrees)");
+        angleY = angleEnter("enter the second angle (in degrees)");
+        chackingMethod(angleX, angleY);
+
+    }
+    private static float angleEnter(String s){
+        System.out.print(s + " >>");
+        float angle;
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNextFloat()) {
+            scanner.nextLine();
+
+        }
+        angle = scanner.nextFloat();
+        return angle;
+    }
+    private static void chackingMethod(float x, float y){
+        float z = 180 - (x+y);
+        if(z<=0){
+            System.out.println("triangle is not exist.");
+        }
+        if(z==90.0 || x==90.0 || y== 90.0){
+            System.out.println("triangle exists. it is right triangle");
+        }
+        else System.out.println("triangle exists.");
+
+    }
+}
