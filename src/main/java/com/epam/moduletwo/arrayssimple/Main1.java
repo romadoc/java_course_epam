9ac3ac7d4a8585c22c9b52af491465c8ddeeb5ae
Roman Doroshenko <coachandconsult3@gmail.com>
2020-12-18 23:46:35 +0300

    module 2, task 1

--- src/main/java/com/epam/moduletwo/arrayssimple/Main1.java
+++ src/main/java/com/epam/moduletwo/arrayssimple/Main1.java
@@ -3,11 +3,11 @@ package com.epam.moduletwo.arrayssimple;
 import com.epam.moduletwo.utils.ScannerUtil;
 
 //В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.
+
 public class Main1 {
     public static void main(String[] args) {
       fillingArray();
     }
-
     static void fillingArray() {
         int a = (int)(1+Math.random()*51);
         int[] arrayOfInteger = new int[a];
@@ -16,7 +16,6 @@ public class Main1 {
         }
         printResult(arrayOfInteger, ScannerUtil.integerIn("Type K (integer)"));
     }
-
     private static void printResult(int[] array, int a) {
         System.out.println("числа, кратные К (при К = "+a+"): ");
         for (int j = 0; j < array.length; j++) {
