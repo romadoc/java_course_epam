b50232e3d0eb3aca35b0251bc016ed368c9197cb
Roman Doroshenko <coachandconsult3@gmail.com>
2020-12-18 22:57:57 +0300

    Revert "второй модуль"
    This reverts commit 3acd2a3f

---
+++ src/main/java/com/epam/moduleone/partonelinear/Main1.java
@@ -0,0 +1,17 @@
+package com.epam.moduleone.partonelinear;
+
+public class Main1 {
+
+    /**
+     * Найдите значение функции: z = ((a – 3 ) * b / 2) + c.
+     * @param args
+     */
+    public static void main(final String[] args) {
+        double a = 2.5;
+        double b = 3.4;
+        double c = 4.2;
+        double z = ((a - 3) * b / 2) + c;
+        System.out.println(z);
+        byte d = 1;
+    }
+}
---
+++ src/main/java/com/epam/moduleone/partthreecycle/Main2.java
@@ -0,0 +1,20 @@
+package com.epam.moduleone.partthreecycle;
+
+//вычислить значения функции y = x, x > 2; y = -x, x <=2, с шагом h(шаг принимаем за 1)
+public class Main2 {
+    public static void main(String[] args) {
+        int y;
+        System.out.println("значения функции при y = x, x > 2:");
+        for (int i = 3; i < 8; i++) {
+            y = i;
+            System.out.print(" x = " + i + " y = " + y + ";");
+        }
+        System.out.println();
+        System.out.println("значения функции при y = -x, x<=2:");
+        for (int i = -2; i <= 2; i++) {
+            y = -i;
+            System.out.print(" x = " + i + " y = " + y + ";");
+
+        }
+    }
+}
