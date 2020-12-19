3d4a0952e8b43a13a56f5b09c6c97b3b278ebdff
Roman Doroshenko <coachandconsult3@gmail.com>
2020-12-19 07:48:22 +0300

    module 2, One-dimensional arrays, task 3

--- src/main/java/com/epam/moduletwo/arrayssimple/Main3.java
+++ src/main/java/com/epam/moduletwo/arrayssimple/Main3.java
@@ -15,23 +15,29 @@ public class Main3 {
     }
 
     private static double[] arrayGenerator() {
+
         int n = ScannerUtil.integerIn("Enter the dimension of the array");
         double[] array = new double[n];
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 200 - 100);
             System.out.print(" " + array[i] + "; ");
         }
+
         System.out.println();
         return array;
     }
     private static void answerGenerator(double[]arrayIn){
+
         for (int i = 0; i < arrayIn.length; i++) {
             if (arrayIn[i] == 0) {
                 countZeroNumbers = countZeroNumbers + 1;
-            } else if (arrayIn[i] > 0) {
+            }
+            else if (arrayIn[i] > 0) {
                 countPositiveNumbers = countPositiveNumbers + 1;
-            } else countNegativeNumbers = countNegativeNumbers + 1;
-
+            }
+            else {
+                countNegativeNumbers = countNegativeNumbers + 1;
+            }
         }
         System.out.println("положительных элементов: " + countPositiveNumbers);
         System.out.println("нулевых элементов: " + countZeroNumbers);
