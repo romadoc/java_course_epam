8c838fd55bc0b301fa5821c49704b151c61c8457
Roman Doroshenko <coachandconsult3@gmail.com>
2020-12-19 17:21:37 +0300

    module 2, part 2 - arrays of arrays, task 1

--- src/main/java/com/epam/moduletwo/arraysofarrays/Main1.java
+++ src/main/java/com/epam/moduletwo/arraysofarrays/Main1.java
@@ -1,4 +1,30 @@
 package com.epam.moduletwo.arraysofarrays;
 
+//Дана матрица. Вывести на экран все нечетные столбцы, у которых первый элемент больше последнего.
+
 public class Main1 {
+    public static void main(String[] args) {
+        //arrayConstructor();
+        decideTask(arrayCration());
+    }
+    private static int[][] arrayCration(){
+        int[][] array = {
+                {9, 2, 3, 4, 5},
+                {3, 4, 6, 7, 8 },
+                {2, 4, 6, 7, 8 },
+                {3, 4, 6, 2, 8 },
+                {3, 4, 6, 7, 1 },
+        };
+        return array;
+    }
+    private static void decideTask(int[][] arrayIn){
+        for (int i = 0; i < arrayIn.length; i++){
+            for (int j = 0; j<arrayIn[i].length; j += 2){
+                if (arrayIn[0][j] > arrayIn[arrayIn.length-1][j] ) {
+                    System.out.print(arrayIn[i][j] + "\t");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
