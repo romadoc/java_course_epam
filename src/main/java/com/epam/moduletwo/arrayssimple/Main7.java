81275752ce635124bd5156ed0834e09a8e37f254
Roman Doroshenko <coachandconsult3@gmail.com>
2021-01-12 22:18:34 +0300

    module2.part1.task7 fixed

--- src/main/java/com/epam/moduletwo/arrayssimple/Main7.java
+++ src/main/java/com/epam/moduletwo/arrayssimple/Main7.java
@@ -1,20 +1,23 @@
 package com.epam.moduletwo.arrayssimple;
+
+import java.util.Arrays;
+import java.util.Collections;
+
 //Даны действительные числа a1, a2,...an найти мах суммы парных чисел.
 // макс из а0+а1 ; а2 + а3;...
 public class Main7 {
     public static void main(String[] args) {
-        int[] massive = arrayGenerator();
+        int[] massive = createArray();
 
         System.out.print("generated array: ");
         for (int elem: massive){
             System.out.print(" " + elem + "; ");
         }
         System.out.println();
-        System.out.println(" max sum of presented pairs is: " + maxFinder(pairFinder(massive)));
-
+       createAnswer(massive);
     }
 
-    private static int[] arrayGenerator() {
+    private static int[] createArray() {
         int n;
         n = (int) (Math.random() * 20);
         if (n % 2 != 0) {
@@ -27,33 +30,21 @@ public class Main7 {
         return massive;
     }
 
-    private static int[] pairFinder(int[] massive) {
+    private static int[] createAnswer(int[] massive) {
 
         int [] arrayForCount = new int[massive.length/2];
         int pairSum;
         int pSumIndex;
         pSumIndex = 0;
-        for (int i = 0; i < massive.length; i++) {
+        for (int i = 0; i < massive.length; i += 2) {
             pairSum = massive[i] + massive[i+1];
             arrayForCount[pSumIndex] = pairSum;
             pSumIndex ++;
-            i = i + 1;
         }
-        System.out.println();
         System.out.print("final array: ");
-        for (int y = 0; y < arrayForCount.length; y++){
-            System.out.print(" " + arrayForCount[y] + "; ");
-        }
+        Arrays.sort(arrayForCount);
+        System.out.println(Arrays.toString(arrayForCount));
+        System.out.println("max sum of presented pairs is: " + arrayForCount[arrayForCount.length-1]);
         return arrayForCount;
-    }
-
-    private static int maxFinder(int [] sourseArray) {
-        int max;
-        max = sourseArray[0];
-        for (int i = 0; i < sourseArray.length; i ++){
-            if (max < sourseArray[i]) max = sourseArray[i];
-        }
-
-        return max;
     }
 }
