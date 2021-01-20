40c5f83b6ff66c037ac7fe2863ba9ed9d8b3fa5e
Roman Doroshenko <coachandconsult3@gmail.com>
2021-01-20 21:46:42 +0300

    module4.part1.task8

---
+++ src/main/java/com/epam/modulefour/taskeight/Customer.java
@@ -0,0 +1,103 @@
+package com.epam.modulefour.taskeight;
+
+import java.text.DecimalFormat;
+
+/**
+ * Создать класс Customer, спецификация которого приведена ниже. Определить конструкторы, set- и get- методы
+ * и метод toString(). Создать второй класс, агрегирующий массив типа Customer, с подходящими конструкторами
+ * и методами. Задать критерии выбора данных и вывести эти данные на консоль.
+ *
+ * Класс Customer: id, фамилия, имя, отчество, адрес, номер кредитной карточки, номер банковского счета.
+ * Найти и вывести:
+ * a) список покупателей в алфавитном порядке;
+ * b) список покупателей, у которых номер кредитной карточки находится в заданном интервале
+ */
+public class Customer {
+   private int id;
+   private String surname;
+   private String name;
+   private String patronymic;
+   private String address;
+   private long cardNumber;
+   private int bankAccountNumber;
+
+    public Customer(int id, String surname, String name, String patronymic, String address,
+                    long cardNumber, int bankAccountNumber)                                   {
+        this.id = id;
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.address = address;
+        this.cardNumber = cardNumber;
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public Customer(String surname, String name, String patronymic, long cardNumber) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.cardNumber = cardNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public long getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public int getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(int bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return  "\n" + surname + " " + name + " " + patronymic + "; " + address + "; card number: " +
+                (new DecimalFormat("####,####").format(cardNumber)) + " ";
+    }
+}
---
+++ src/main/java/com/epam/modulefour/taskeight/CustomersSorter.java
@@ -0,0 +1,33 @@
+package com.epam.modulefour.taskeight;
+
+import com.epam.utils.ScannerUtil;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class CustomersSorter {
+    List<Customer>customers;
+    CustomersSorter(List<Customer>customers){
+        this.customers = customers;
+    }
+
+   public void sortCustomersBySurnames(){
+        Collections.sort(customers, new Comparator<Customer>() {
+            @Override
+            public int compare(Customer customer1, Customer customer2) {
+                return customer1.getSurname().compareTo(customer2.getSurname());
+            }
+        });
+   }
+
+    public void sortCustomersFromNumToNum(){
+        long numberFrom = ScannerUtil.longIn("from card number");
+        long numberTo = ScannerUtil.longIn("to card number");
+
+        for (int i = 0; i < customers.size(); i++){
+           if(customers.get(i).getCardNumber() <= numberTo&&customers.get(i).getCardNumber()>=numberFrom){
+               System.out.println(customers.get(i));
+           }
+        }
+    }
+}
