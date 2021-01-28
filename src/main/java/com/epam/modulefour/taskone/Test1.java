27f13e9823d7cf59656abe23602fc4ff461b9c4d
Roman Doroshenko <coachandconsult3@gmail.com>
2021-01-28 11:16:53 +0300

    module4.task1

--- src/main/java/com/epam/modulefour/taskone/Test1.java
+++ src/main/java/com/epam/modulefour/taskone/Test1.java
@@ -17,23 +17,27 @@ public class Test1 {
     public void setB(int b) {
         this.b = b;
     }
+
     public void printData(){
         System.out.println("Class Test1:");
         System.out.println("first variable "+a+" "+"second variable "+b);
         System.out.println("sum: " + sumAandB(a,b));
         System.out.println("max digit is: " + dеfineMaxDigit(a,b));
     }
+
     private int sumAandB(int a, int b) {
         this.a = a;
         this.b = b;
         return a+b;
     }
+
     private int dеfineMaxDigit(int a, int b) {
         this.a = a;
         this.b = b;
         return Math.max(a, b);
     }
 }
+
 class Test{
     public static void main(String[] args) {
         Test1 test1 = new Test1();
