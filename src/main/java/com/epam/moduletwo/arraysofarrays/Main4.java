e433751070e071f18f881cee6fc1b70e7cf7001c
Roman Doroshenko <coachandconsult3@gmail.com>
2021-02-12 20:48:19 +0300

    syntax is fixed

--- src/main/java/com/epam/moduletwo/arraysofarrays/Main4.java
+++ src/main/java/com/epam/moduletwo/arraysofarrays/Main4.java
@@ -11,19 +11,16 @@ public class Main4 {
 
     }
     private static int[][] createMatrix() {
-        int x;
-        x = 10;
+        int x = 10;
         int[][] array = new int[x][x];
-        int n;
-        n = 10;
+        int n = 10;
         int n1 = 1;
         for (int i = 0; i < array.length; i++) {
-
-            for (int j = 0; j < array[i].length; j = j+2) {
+            for (int j = 0; j < array[i].length; j = j + 2) {
                 array[j][i] = n1;
             }
             n1++;
-            for (int y = 1; y < array[i].length; y = y+2) {
+            for (int y = 1; y < array[i].length; y = y + 2) {
                 array[y][i] = n;
             }
             n--;
