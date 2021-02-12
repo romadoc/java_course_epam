8d6b04c6efbeaf35148be0b2e8559d3044159836
Roman Doroshenko <coachandconsult3@gmail.com>
2021-02-12 19:41:18 +0300

    module2 task 3 fixed

--- src/main/java/com/epam/moduletwo/arraysofarrays/Main3.java
+++ src/main/java/com/epam/moduletwo/arraysofarrays/Main3.java
@@ -5,22 +5,22 @@ package com.epam.moduletwo.arraysofarrays;
 import com.epam.utils.ScannerUtil;
 public class Main3 {
     public static void main(String[] args) {
-        int [][] array;
-        array = createMatrix();
+        int n = (int)(Math.random() * 10);
+        int m = (int)(Math.random() * 10);
+        int [][] array = createMatrix(n, m);
         int lineNumber = ScannerUtil.integerIn("Enter line number (positive integer)");
         int columnNumber = ScannerUtil.integerIn("Enter column number(positive integer)");
-        printOutLine(lineNumber, columnNumber, array);
+        printOutLine(lineNumber, columnNumber, array, m);
     }
 
-    private static int[][] createMatrix() {
-        int n;
-        n = (int)(Math.random()*10);
-        int[][] array = new int[n][n];
+    private static int[][] createMatrix(int n, int m) {
+
+        int [][] array = new int[n][m];
         System.out.println("Generated Matrix");
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++){
 
             for (int j = 0; j < array[i].length; j++) {
-                array[i][j] = (int)(Math.random()*20);
+                array[i][j] = (int)(Math.random() * 20);
                 System.out.print(array[i][j] + "\t");
 
             }
@@ -30,18 +30,16 @@ public class Main3 {
         return array;
     }
 
-    private static void printOutLine(int x, int y, int [][]arrayInput) {
+    private static void printOutLine(int lineNum, int coloumnNum, int [][]arrayInput, int m) {
         System.out.print("selected line: ");
-        for (int i = 0; i < arrayInput.length; i++) {
-
-            System.out.print(arrayInput[x][i] + "; ");
-
+        for (int i = 0; i < m; i++) {
+            System.out.print(arrayInput[lineNum][i] + "; ");
         }
         System.out.println();
         System.out.print("selected column: ");
         for (int i = 0; i < arrayInput.length; i++) {
 
-            System.out.print(arrayInput[i][y] + "; ");
+            System.out.print(arrayInput[i][coloumnNum] + "; ");
 
         }
     }
