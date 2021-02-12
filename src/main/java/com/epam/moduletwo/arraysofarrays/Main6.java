61deb5d9a97f4a8a3511c6979d0dcfe93f848546
Roman Doroshenko <coachandconsult3@gmail.com>
2021-02-12 20:50:37 +0300

    syntax is fixed

--- src/main/java/com/epam/moduletwo/arraysofarrays/Main6.java
+++ src/main/java/com/epam/moduletwo/arraysofarrays/Main6.java
@@ -21,14 +21,14 @@ public class Main6 {
         int x = 0;
         int y = 0;
         for (int i = 0; i < array.length; i++) {
-            for (int j = x; j < array[i].length-x; j++) {
+            for (int j = x; j < array[i].length - x; j++) {
                 array[i][j] = 1;
             }
             x++;
 
         }
-        for (int i = array.length-1; i >= array.length/2; i--) {
-            for (int j = y; j < array[i].length-y; j++ ) {
+        for (int i = array.length-1; i >= array.length / 2; i--) {
+            for (int j = y; j < array[i].length - y; j++ ) {
                 array[i][j] = 1;
             }
             y++;
