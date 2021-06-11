65a30d1cb1892a57f7ed587a3ec3ad7716b5b443
Roman <68703412+romadoc@users.noreply.github.com>
2021-06-11 13:25:23 +0300

    Delete src/main/java/com/epam/advancedcourse/gitdemo directory

--- src/main/java/com/epam/advancedcourse/gitdemo/TestGitDemo.java
+++
@@ -1,8 +0,0 @@
-package com.epam.advancedcourse.gitdemo;
-
-public class TestGitDemo {
-    public static void main(String[] args) {
-        GitDemo gitDemo = new GitDemo();
-        gitDemo.printGreeting();
-    }
-}
